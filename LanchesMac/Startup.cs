e34cb7fc0a97a4b80ea39823c5a6b4e7c029b8fe
Renata Rodrigues <ticare1@hotmail.com>
2023-02-08 20:32:45 -0300

    Criado o Controlador para gerenciar os itens do carrinho de compras - parte 1

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -19,8 +20,8 @@ public class Startup
         
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
-
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
 
         services.AddControllersWithViews();   
         services.AddMemoryCache();
