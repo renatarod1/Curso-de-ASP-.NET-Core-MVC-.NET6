37c034335128e47a4c8587f0c49d0ca5b0efd598
Renata Rodrigues <ticare1@hotmail.com>
2023-02-20 13:51:09 -0300

    Criado o método action Checkout HttpPost

--- LanchesMac/Controllers/PedidoController.cs
+++ LanchesMac/Controllers/PedidoController.cs
@@ -20,7 +20,35 @@ namespace LanchesMac.Controllers
 
         [HttpPost]
         public IActionResult Checkout(Pedido pedido) {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            List<CarrinhoCompraItem> itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            if (_carrinhoCompra.CarrinhoCompraItems.Count == 0) {
+                ModelState.AddModelError("", "Seu carrinho esta vazio, que tal incluir um lanche...");
+            }
+
+            foreach (var item in itens) {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            if (ModelState.IsValid) {
+                _pedidoRepository.CriarPedido(pedido);
+                
+                ViewBag.CheackoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                _carrinhoCompra.LimparCarrinho();
+
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
         }
     }
 }
