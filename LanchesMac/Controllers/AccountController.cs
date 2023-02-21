17b572d10baffac4c31023abb8d7e8ecc76de6cb
Renata Rodrigues <ticare1@hotmail.com>
2023-02-21 18:13:30 -0300

    Implementado o Logout

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -61,5 +61,13 @@ namespace LanchesMac.Controllers
             }
             return View(registroVM);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout() {
+            HttpContext.Session.Clear();
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
