e76c3de5f4922d9fce2ff69f672db19dfe47b0c5
Renata Rodrigues <ticare1@hotmail.com>
2023-03-04 13:44:05 -0300

    Criada a View VendasMensal para gerar o gráfico de colunas

--- LanchesMac/Areas/Admin/Controllers/AdminGraficoController.cs
+++ LanchesMac/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -22,17 +22,17 @@ namespace LanchesMac.Areas.Admin.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index(int dias) {
+        public IActionResult Index() {
             return View();
         }
 
         [HttpGet]
-        public IActionResult VendasMensal(int dias) {
+        public IActionResult VendasMensal() {
             return View();
         }
 
         [HttpGet]
-        public IActionResult VendasSemanal(int dias) {
+        public IActionResult VendasSemanal() {
             return View();
         }
     }
