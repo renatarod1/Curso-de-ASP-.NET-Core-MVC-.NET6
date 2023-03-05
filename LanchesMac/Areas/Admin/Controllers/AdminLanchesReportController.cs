b7cae8c8de80c2e663507b2faa569f4197a92cad
Renata Rodrigues <ticare1@hotmail.com>
2023-03-05 19:30:45 -0300

    Implementada a geração do relatório em formato PDF

--- LanchesMac/Areas/Admin/Controllers/AdminLanchesReportController.cs
+++ LanchesMac/Areas/Admin/Controllers/AdminLanchesReportController.cs
@@ -1,4 +1,5 @@
 ﻿using FastReport.Data;
+using FastReport.Export.PdfSimple;
 using FastReport.Web;
 using LanchesMac.Areas.Admin.FastReportUtils;
 using LanchesMac.Services;
@@ -31,6 +32,30 @@ namespace LanchesMac.Areas.Admin.Controllers
             webReport.Report.RegisterData(lanches, "LanchesReport");
             webReport.Report.RegisterData(categorias, "CategoriasReport");
             return View(webReport);
+        }
+
+        public async Task<ActionResult> LanchesCategoriaPDF() {
+            var webReport = new WebReport();
+            var mssqlDataConnection = new MsSqlDataConnection();
+
+            webReport.Report.Dictionary.AddChild(mssqlDataConnection);
+
+            webReport.Report.Load(Path.Combine(_webHostEnv.ContentRootPath, "wwwroot/reports", "lanchesCategoria.frx"));
+
+            var lanches = HelperFastReport.GetTable(await _relatorioLanchesService.GetLanchesReport(), "LanchesReport");
+            var categorias = HelperFastReport.GetTable(await _relatorioLanchesService.GetCategoriasReport(), "CategoriasReport");
+
+            webReport.Report.RegisterData(lanches, "LanchesReport");
+            webReport.Report.RegisterData(categorias, "CategoriasReport");
+
+            webReport.Report.Prepare();
+            Stream stream = new MemoryStream();
+
+            webReport.Report.Export(new PDFSimpleExport(), stream);
+            stream.Position = 0;
+
+            //return File(stream, "application/zip", "LancheCategoria.pdf"); *** Download
+            return new FileStreamResult(stream, "application/pdf");
         }
     }
 }
